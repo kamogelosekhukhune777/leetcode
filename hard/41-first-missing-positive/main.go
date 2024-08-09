c9b5ad906cdc612a85f9c5c784e87b9921653730
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2024-08-09 23:27:17 +0200

    41. first missing positive

--- hard/41-first-missing-positive/main.go
+++ hard/41-first-missing-positive/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
 
@@ -27,20 +29,29 @@ Constraints:
 */
 
 func firstMissingPositive(nums []int) int {
-	//sort to rearrange the elements
-	for i := 0; i < len(nums); {
-		if nums[i] > 0 && nums[i] <= len(nums) && nums[i] != nums[nums[i]-1] {
-			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
-		} else {
-			i++
+	n := len(nums)
+
+	// Step 1: Place each number in its correct position if possible
+	for i := 0; i < n; i++ {
+		for nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i] {
+			nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
 		}
 	}
 
-	for i, num := range nums {
-		if num != i+1 {
+	// Step 2: Find the first missing positive integer
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
 			return i + 1
 		}
 	}
 
-	return len(nums) + 1
+	// Step 3: If all positions are correctly placed, the missing integer is n+1
+	return n + 1
+}
+
+func main() {
+	// Test cases
+	fmt.Println(firstMissingPositive([]int{1, 2, 0}))         // Output: 3
+	fmt.Println(firstMissingPositive([]int{3, 4, -1, 1}))     // Output: 2
+	fmt.Println(firstMissingPositive([]int{7, 8, 9, 11, 12})) // Output: 1
 }
