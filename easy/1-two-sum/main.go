cdee9549177c732fea95afde22340c7abdaf1424
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2024-10-10 12:53:44 +0200

    1. two sum

---
+++ easy/1-two-sum/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers
+such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the
+same element twice.
+
+You can return the answer in any order.
+
+
+Example 1:
+
+	Input: nums = [2,7,11,15], target = 9
+	Output: [0,1]
+	Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+	Input: nums = [3,2,4], target = 6
+	Output: [1,2]
+
+Example 3:
+
+	Input: nums = [3,3], target = 6
+	Output: [0,1]
+
+
+Constraints:
+
+	2 <= nums.length <= 104
+	-109 <= nums[i] <= 109
+	-109 <= target <= 109
+	Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		compliment := target - num
+
+		if index, found := numMap[compliment]; found {
+			return []int{index, i}
+		}
+
+		numMap[num] = i
+
+	}
+
+	return nil
+}
+
+func main() {
+	//Example 1:
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(nums, target)) //Output: [0,1]
+
+	//Example 2:
+	nums = []int{3, 2, 4}
+	target = 6
+	fmt.Println(twoSum(nums, target)) //Output: [1,2]
+
+	//Example 3:
+	nums = []int{3, 3}
+	target = 6
+	fmt.Println(twoSum(nums, target)) //Output: [0,1]
+}
