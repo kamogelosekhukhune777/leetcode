b50618b819dab957a69d7c19a8c3a3b0bad1d8f1
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2025-08-08 02:08:00 +0200

    47. Permutations II

---
+++ python/medium/47.permutations-ii.py
@@ -0,0 +1,45 @@
+"""
+47. Permutations II
+
+Given a collection of numbers, nums, that might contain duplicates, return 
+all possible unique permutations in any order.
+
+ 
+Example 1:
+    Input: nums = [1,1,2]
+    Output:
+    [[1,1,2],
+    [1,2,1],
+    [2,1,1]]
+
+Example 2:
+    Input: nums = [1,2,3]
+    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+
+Constraints:
+    1 <= nums.length <= 8
+    -10 <= nums[i] <= 10
+"""
+
+from collections import Counter
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        res = []
+        counter = Counter(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for num in counter:
+                if counter[num] > 0:
+                    path.append(num)
+                    counter[num] -= 1
+                    backtrack(path)
+                    path.pop()
+                    counter[num] += 1
+
+        backtrack([])
+        return res
