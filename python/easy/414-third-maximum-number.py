99ed0ecfda3be94005c6c8ef4431143347e658d6
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2025-10-02 23:35:27 +0200

    414. Third Maximum Number

---
+++ python/easy/414-third-maximum-number.py
@@ -0,0 +1,52 @@
+"""
+414. Third Maximum Number
+
+Given an integer array nums, return the third distinct maximum number in this array. If the third 
+maximum does not exist, return the maximum number.
+
+
+Example 1:
+    Input: nums = [3,2,1]
+    Output: 1
+    Explanation:
+        The first distinct maximum is 3.
+        The second distinct maximum is 2.
+        The third distinct maximum is 1.
+
+Example 2:
+    Input: nums = [1,2]
+    Output: 2
+    Explanation:
+        The first distinct maximum is 2.
+        The second distinct maximum is 1.
+        The third distinct maximum does not exist, so the maximum (2) is returned instead.
+
+Example 3:
+    Input: nums = [2,2,3,1]
+    Output: 1
+    Explanation:
+        The first distinct maximum is 3.
+        The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+        The third distinct maximum is 1.
+
+
+Constraints:
+    1 <= nums.length <= 10^4
+    -2^31 <= nums[i] <= 2^31 - 1
+"""
+import heapq
+
+class Solution(object):
+    def thirdMax(self, nums):
+        seen = set()
+        min_heap = []
+
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+                heapq.heappush(min_heap, num)
+                if len(min_heap) > 3:
+                    heapq.heappop(min_heap)
+        if len(min_heap) == 3:
+            return min_heap[0]
+        return max(min_heap)
